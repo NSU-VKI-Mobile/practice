6bb1171e2d96cb537024d306bdae564cb6d41a88
bykovam <bykovam@mer.ci.nsu.ru>
2026-02-06 13:44:43 +0700

    Задание 2

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -4,15 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ci.nsu.moble.main.ui.theme.Blue
+import ci.nsu.moble.main.ui.theme.Cyan
+import ci.nsu.moble.main.ui.theme.Green
+import ci.nsu.moble.main.ui.theme.Magenta
 import ci.nsu.moble.main.ui.theme.PracticeTheme
+import ci.nsu.moble.main.ui.theme.Red
+import ci.nsu.moble.main.ui.theme.Yellow
+import androidx.compose.runtime.*
+import androidx.compose.material3.TextField
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
+private val colorsMap = mapOf(
+    "Red" to Red,
+    "Green" to Green,
+    "Blue" to Blue,
+    "Yellow" to Yellow,
+    "Cyan" to Cyan,
+    "Magenta" to Magenta,
+)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +57,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun StringInColor(str: String): Color
+{
+    val color = colorsMap.get(str)
+    if (color == null)
+    {
+        println("Нет такого цвета")
+        return Red
+    }
+    else
+        return color
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    var inputColor by remember { mutableStateOf("Red") }
+    var buttonColor by remember { mutableStateOf(StringInColor(inputColor)) }
+    Column(modifier = Modifier.padding(16.dp)) {
+        OutlinedTextField(
+            value = inputColor,
+            onValueChange = { newText -> inputColor = newText },
+            label = { Text("Введите текст") }
+        )
+        Button(
+            onClick = { buttonColor = StringInColor(inputColor) },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = buttonColor
+            )
+        ) {
+            Text("Применить цвет")
+        }
+        colorsMap.forEach { key,value ->
+            Text(
+                text = key
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
