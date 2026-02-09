afdb821d546531be17925212bdaff0a014dd8db2
towa <..>
2026-02-09 18:56:38 +0700

    YIPPPEEEE я все ещще почти ниче не понимаю но чето понимаю

--- main/src/main/java/ci/nsu/moble/main/Lab1.kt
+++ main/src/main/java/ci/nsu/moble/main/Lab1.kt
@@ -2,9 +2,11 @@ package ci.nsu.moble.main
 
 import android.R
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -19,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import ci.nsu.moble.main.ui.theme.PracticeTheme
 
 class Lab1 : ComponentActivity() {
@@ -38,12 +41,12 @@ fun ColorsApp()
 {
     var pickedcolor by remember { mutableStateOf(Color.White) }
     val rainbowColors = mapOf(
-        "Red" to Color(0xFFEF4D60),
-        "Orange" to Color(0xFFFF9156),
-        "Yellow" to Color(0xFFF9FF3D),
-        "Green" to Color(0xFF8AFC70),
-        "Blue" to Color(0xFF4DFFF9),
-        "Violet" to Color(0xFF8F59F5)
+        "red" to Color(0xFFEF4D60),
+        "orange" to Color(0xFFFF9156),
+        "yellow" to Color(0xFFF9FF3D),
+        "green" to Color(0xFF8AFC70),
+        "blue" to Color(0xFF4DFFF9),
+        "violet" to Color(0xFFB694FF)
     )
     var text by remember {mutableStateOf("")}
     Surface(
@@ -58,20 +61,58 @@ fun ColorsApp()
                 value = text,
                 onValueChange = {newText: String ->
                     text = newText},
-                label = {Text("Find color")},
+                label = {Text("find color")},
                 modifier = Modifier.wrapContentSize()
                 //.fillMaxWidth(0.8f)
                 //.fillMaxHeight(0.1f)
             )
             Button(
                 onClick = {
-                    pickedcolor = Color.Cyan
+                    val found = rainbowColors.entries.find {(key, _) ->
+                        key.contains(text.lowercase(), ignoreCase = true)
+                    }
+                    if (found != null)
+                    {
+                        pickedcolor = found.value
+                    }
+                    else {
+                        Log.e("colors", "couldn't find color '$text'")
+                    }
+                    //pickedcolor = Color.Cyan
                 },
                 modifier = Modifier.wrapContentSize()
             ) {
                 Text("Change")
             }
+            ColorCardsList(rainbowColors)
         }
 
+    }
+}
+
+@Composable
+fun ColorCardsList(colorMap: Map<String, Color>) {
+    Column (
+        Modifier.width(100.dp)
+    ){
+        colorMap.forEach { (name, color) ->
+            ColorCard(name = name, color = color)
+        }
+    }
+}
+
+@Composable
+fun ColorCard(name: String, color: Color) {
+    Card(
+        modifier = Modifier
+            .background(color)
+            .fillMaxWidth(),
+    ) {
+        Text(
+            text = name,
+            modifier = Modifier
+                .background(color)
+                .fillMaxWidth()
+        )
     }
 }
