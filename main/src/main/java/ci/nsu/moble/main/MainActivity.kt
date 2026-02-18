a885f663ac7b7359e195560671b7008b30c1f87a
bl1nd3r-pub <babikovartem2007@gmail.com>
2026-02-19 01:30:15 +0700

    Выполнена лаб. 3 - реализованы переходы между двумя активити через интент, а также переходы между экранами через композ навигацию

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,15 +1,19 @@
 package ci.nsu.moble.main
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -47,10 +51,22 @@ fun MainScreenActivity(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // TODO:  нужно добавить  TextField
+        TextField(
+            value = text,
+            onValueChange = {text = it},
+            label = {Text("Введите текст")},
+            modifier = Modifier.fillMaxWidth()
+        )
         Button(
             onClick = {
-                // TODO:  нужно добавить кнопку которая по клику открывает второе активити через интент
+                val intent = Intent(context, SecondActivity::class.java).apply {
+                    putExtra("text_data", text) // передаём строку
+                }
+                if (context is Activity) {
+                    context.startActivity(intent)
+                } else {
+                    // Для Preview — игнорируем
+                }
             },
             modifier = Modifier.padding(top = 16.dp)
         ) {
