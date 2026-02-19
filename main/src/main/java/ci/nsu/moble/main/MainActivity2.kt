1ed1d1253ceca588b58d2cbf7b852572c7b330ff
livitchuksa <livitchuksa@mer.ci.nsu.ru>
2026-02-19 11:39:50 +0700

    Done textfield and button

--- main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.TextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color as c
+import androidx.compose.ui.unit.sp
 
 @Stable
 val Orange = c(color = 0xFF910A)
@@ -37,9 +51,7 @@ class MainActivity2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-//            PeopleTheme{
-//                Main()
-//            }
+            Main()
         }
     }
 }
@@ -47,5 +59,27 @@ class MainActivity2 : ComponentActivity() {
 @Composable
 fun Main() {
     var text by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement=Arrangement.Top,
+        horizontalAlignment=Alignment.CenterHorizontally
+    ){
+        TextField(
+            value = text,
+            onValueChange = {newText -> text=newText},
+            label = {Text("Введите цвет")},
+            modifier= Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { text = text },
+            modifier = Modifier
+                .height(height = 48.dp)
+                .fillMaxWidth()){
+            Text(text = "Применить цвет")
+        }
+    }
 }
 
