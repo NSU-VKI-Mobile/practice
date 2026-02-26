2757901038ee4d436f80cc1bcf2fb09826fd9ba5
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-02-26 11:18:13 +0700

    Начальный шаблон

---
+++ main/src/main/java/ci/nsu/moble/main/ui/ShoppingScreen.kt
@@ -0,0 +1,14 @@
+package ci.nsu.moble.main.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ci.nsu.moble.main.ui.theme.PracticeTheme
+
+@Composable
+fun ShoppingScren() {
+    Text(
+        text = "Hello"
+    )
+}
