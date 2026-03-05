fe8f2b2cabae0a4d62683f295db747c745eef709
Ta4erOK <dagienko79@gmail.com>
2026-03-05 11:26:31 +0700

    Task-1

--- main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ci.nsu.moble.main.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
