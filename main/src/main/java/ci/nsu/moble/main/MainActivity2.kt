9197c47d39cfb03ee913e6f9677aab8e481b0b27
Ta4erOK <dagienko79@gmail.com>
2026-03-05 11:45:42 +0700

    Task-1

--- main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
@@ -8,7 +8,7 @@ class MainActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
