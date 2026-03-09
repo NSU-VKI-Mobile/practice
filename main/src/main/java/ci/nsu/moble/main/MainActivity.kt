213507c73bbfb3511c47d2764834900770b7ed50
Mikhail Sklyar <work@miksklyar.ru>
2026-03-09 19:45:03 +0700

    add text field + navigation to SecondActivity

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package ci.nsu.moble.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,16 +11,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ci.nsu.moble.main.ui.theme.PracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,22 +39,32 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 // TODO:  here is to open the second activity
 @Composable
 fun MainScreenActivity(modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("") }
+    val text = remember { mutableStateOf("") }
     val context = LocalContext.current
 
     Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // TODO:  нужно добавить  TextField
+        TextField(
+            value = text.value,
+            textStyle = TextStyle(fontSize = 25.sp),
+            placeholder = { Text("Введите текст") },
+            onValueChange = { newText ->
+                text.value = newText
+            })
         Button(
             onClick = {
-                // TODO:  нужно добавить кнопку которая по клику открывает второе активити через интент
-            },
-            modifier = Modifier.padding(top = 16.dp)
+                val intent = Intent(context, SecondActivity::class.java)
+                intent.putExtra("text_data", text.value)
+                context.startActivity(intent)
+            }, modifier = Modifier.padding(top = 16.dp)
         ) {
             Text("Open SecondActivity")
         }
