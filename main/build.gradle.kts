95a4a47f3da3f6c11f211bfbac3cbd8d8d5377cb
nilaw <nilawyn202@gmail.com>
2026-03-18 20:48:55 +0900

    re-uploaded

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -33,12 +34,28 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.appcompat)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -1,41 +1,121 @@
 package ci.nsu.mobile.main
 
-import androidx.core.content.ContextCompat
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
-import java.lang.reflect.Field
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContent {
+                MainScreen()
+        }
+    }
+}
 
-        val editTextColorName = findViewById<EditText>(R.id.editTextColorName)
-        val buttonApplyColor = findViewById<Button>(R.id.buttonApplyColor)
-        val colorNames = listOf("red", "orange", "yellow", "green", "blue", "indigo", "violet")
-
-        buttonApplyColor.setOnClickListener {
-            val colorName = editTextColorName.text.toString().trim().lowercase()
-
-            if (colorName.isEmpty()) {
-                return@setOnClickListener
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
+        NavigationBar {
+            NavigationBarItem(
+                icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                label = { Text("Главная") },
+                selected = navController.currentDestination?.route == State.Main.route,
+                onClick = {
+                    navController.navigate(State.Main.route) {
+                        // Очищаем стек до этого пункта, чтобы не копить экраны
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                })
+            NavigationBarItem(
+                icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                label = { Text("Дополнительная") },
+                selected = navController.currentDestination?.route == State.Second.route,
+                onClick = {
+                    navController.navigate(State.Second.route) {
+                        // Очищаем стек до этого пункта, чтобы не копить экраны
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                })
+        }
+    }) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = State.Main.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(State.Main.route) {
+                HomeScreen(innerPadding)
             }
-
-            if (colorName in colorNames) {
-                try {
-                    val field: Field = R.color::class.java.getField(colorName)
-                    val colorId = field.getInt(null)
-                    val color = ContextCompat.getColor(this, colorId)
-                    buttonApplyColor.setBackgroundColor(color)
-                } catch (e: Exception) {
-                    Log.e("MainActivity", "Error setting color: ", e)
-                }
-            } else {
-                Log.d("MainActivity", "Пользовательский цвет \"$colorName\" не найден")
+            composable(State.Second.route) {
+                SecondScreen("privet")
             }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(innerPadding: PaddingValues) {
+    val context = LocalContext.current
+    var inputText by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = inputText,
+            onValueChange = { inputText = it },  // Обновляем состояние при вводе
+            label = { Text("Введите текст") },
+        )
+        Button(
+            onClick = {
+                val intent = Intent(context, SecondActivity::class.java)
+                intent.putExtra("EXTRA_MESSAGE", inputText)
+                context.startActivity(intent)
+            }, modifier = Modifier.padding(innerPadding)
+        ) {
+            Text("Перейти", modifier = Modifier.padding(innerPadding))
         }
     }
 }
