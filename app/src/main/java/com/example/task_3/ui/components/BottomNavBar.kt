ac0643df0223b598a77e7bb0f283bd8be52ef689
kulishsa <kulishsa@mer.ci.nsu.ru>
2026-03-19 19:00:15 +0300

    Task 3

---
+++ app/src/main/java/com/example/task_3/ui/components/BottomNavBar.kt
@@ -0,0 +1,35 @@
+package com.example.task_3.ui.components
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.task_3.navigation.Screen
+
+@Composable
+fun BottomNavBar(
+    navController: NavHostController
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    NavigationBar {
+        Screen.items.forEach { screen ->
+            NavigationBarItem(
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = { Icon(screen.icon, contentDescription = screen.title) },
+                label = { Text(screen.title) }
+            )
+        }
+    }
+}
