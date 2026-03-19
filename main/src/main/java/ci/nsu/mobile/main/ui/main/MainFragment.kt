ff4b94351d341dd2b3aae8ca9c9d51bbfbeb9833
hgfep <hgfep@ANTON>
2026-03-19 18:21:08 +0700

    Task_1_2/2

---
+++ main/src/main/java/ci/nsu/mobile/main/ui/main/MainFragment.kt
@@ -0,0 +1,23 @@
+package ci.nsu.mobile.main.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ci.nsu.mobile.main.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
