d6f4b299da27f80dbcdd8a11e5b92214f10536ad
smachniybober228 <ilyadobrov07@gmail.com>
2026-03-19 14:20:52 +0700

    End lab

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
     testImplementation("junit:junit:4.13.2")
 
     // Заменяем старые тестовые библиотеки
--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import ci.nsu.moble.main.ui.theme.PracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,34 +34,60 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             PracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                    ){
-                        Text("Test")
-                        var sliderr by remember { mutableFloatStateOf(0f) }
-                        var sliderg by remember { mutableFloatStateOf(0f) }
-                        var sliderb by remember { mutableFloatStateOf(0f) }
-                        Slider(
-                            value = sliderr,
-                            onValueChange = { sliderr = it },
-                            valueRange = 0f..255f
-                        )
-                        Slider(
-                            value = sliderg,
-                            onValueChange = { sliderg = it },
-                            valueRange = 0f..255f)
-                        Slider(
-                            value = sliderb,
-                            onValueChange = { sliderb = it },
-                            valueRange = 0f..255f
-                        )
-                    }
-                }
+                MainScreen()
+            }
+        }
+    }
+
+    @Composable
+    fun MainScreen(){
+        val viewModel: ColorPickerViewModel = viewModel()
+        val uiState by viewModel.uiState.collectAsState()
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Box( modifier = Modifier
+                    .size(100.dp)
+                    .background(uiState.color))
+                Text(uiState.hexCode,
+                    color = uiState.color,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp)
+                )
+                Slider(
+                    value = uiState.red.toFloat(),
+                    onValueChange = { viewModel.onRedChanged(it)},
+                    valueRange = 0f..255f,
+                    modifier = Modifier.padding(start = 32.dp, end = 32.dp, top = 16.dp, bottom = 16.dp)
+                )
+                Slider(
+                    value = uiState.green.toFloat(),
+                    onValueChange = { viewModel.onGreenChanged(it)},
+                    valueRange = 0f..255f,
+                    modifier = Modifier.padding(start = 32.dp, end = 32.dp, top = 16.dp, bottom = 16.dp))
+                Slider(
+                    value = uiState.blue.toFloat(),
+                    onValueChange = { viewModel.onBlueChanged(it)},
+                    valueRange = 0f..255f,
+                    modifier = Modifier.padding(start = 32.dp, end = 32.dp, top = 16.dp, bottom = 16.dp)
+                )
+
+                Button(
+                    onClick = { viewModel.generateRandomColor() },
+                ) { Text("Случайный цвет",
+                    fontSize = 20.sp) }
             }
         }
     }
