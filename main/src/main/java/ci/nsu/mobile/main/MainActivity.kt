34b3c15f7dd834a50090612fd65771ec4368496f
smachniybober228 <ilyadobrov07@gmail.com>
2026-03-24 11:24:49 +0700

    Start lab

---
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -0,0 +1,76 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package ci.nsu.mobile.main
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import ci.nsu.mobile.main.ui.theme.PracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar =
+                    {
+                        TopAppBar(
+                            title = { Text("Расчёт вкладов") },          // Заголовок (Composable)
+//                            navigationIcon = {                       // Иконка слева (например, меню или назад)
+//                                IconButton(onClick = { /* обработчик */ }) {
+//                                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Назад")
+//                                }
+//                            },
+//                            actions = {                               // Список действий справа
+//                                IconButton(onClick = { /* поиск */ }) {
+//                                    Icon(Icons.Default.Search, contentDescription = "Поиск")
+//                                }
+//                                IconButton(onClick = { /* настройки */ }) {
+//                                    Icon(Icons.Default.MoreVert, contentDescription = "Ещё")
+//                                }
+//                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary, // фон
+                                titleContentColor = MaterialTheme.colorScheme.onPrimary, // цвет заголовка
+                                navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                                actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+                            ),
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }) { innerPadding ->
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally, // центрируем по горизонтали
+                        verticalArrangement = Arrangement.Center) {
+                        Button(onClick = {
+
+                            val intent = Intent(this, MainInputActivity::class.java)
+                            startActivity(intent)
+                        }, modifier = Modifier.padding(innerPadding)) { Text("Рассчитать") }
+                        Button(onClick = {}, modifier = Modifier.padding(innerPadding)) { Text("История рассчётов") }
+                        Button(onClick = {}, modifier = Modifier.padding(innerPadding)) { Text("Закрыть приложение") }
+                    }
+                }
+            }
+        }
+    }
+}
