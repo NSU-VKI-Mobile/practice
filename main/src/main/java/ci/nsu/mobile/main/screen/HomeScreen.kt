012d42b53164e7a600df1d7ed39da2ddbe28cae2
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-03-26 00:28:15 +0700

    Реализация HomeScreen.kt: -объявляем функцию (с большой буквы, ибо подобные функции ближе к классам, так как по сути не выполняют какую-то логику, а являются частью построения) -объявляем параметры onCalculateClick, onHistoryClick, onExitClick для обработки нажатия кнопок на главном экране(Рассчитать, История расчётов и Закрыть приложение соответсвенно)

---
+++ main/src/main/java/ci/nsu/mobile/main/screen/HomeScreen.kt
@@ -0,0 +1,12 @@
+package ci.nsu.mobile.main.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(
+    onCalculateClick: () -> Unit = {},
+    onHistoryClick: () -> Unit = {},
+    onExitClick: () -> Unit = {}
+) {
+
+}
