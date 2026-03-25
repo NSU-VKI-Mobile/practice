2612bededc67dcf66bba027b01a3b6dc6fbab2c8
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-03-26 00:32:50 +0700

    к главному экрану подкрутили PracticeTheme - тема, созданная android studio(внутри задаются цвета и шрифты) -(Все экраны должны быть внутри PracticeTheme, чтобы выглядеть одинаково_
    -------------------------------
    Добавили контейнер для вертикального расположения(с отцентровкой по горизонтали)

--- main/src/main/java/ci/nsu/mobile/main/screen/HomeScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/screen/HomeScreen.kt
@@ -1,6 +1,11 @@
 package ci.nsu.mobile.main.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import ci.nsu.mobile.main.ui.theme.PracticeTheme
 
 @Composable
 fun HomeScreen(
@@ -8,5 +13,12 @@ fun HomeScreen(
     onHistoryClick: () -> Unit = {},
     onExitClick: () -> Unit = {}
 ) {
-
+    PracticeTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // пупупу
+        }
+    }
 }
