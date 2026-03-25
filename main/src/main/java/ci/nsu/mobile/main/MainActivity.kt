a4bb40b68aff2231e919b38d99aa1eb13bb571d3
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-03-26 00:51:15 +0700

    в MainActivity.kt реализован выход из приложения(передали встроенную функцию finish как параметр)

--- main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -5,13 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ci.nsu.mobile.main.screen.HomeScreen
 import ci.nsu.mobile.main.ui.theme.PracticeTheme
 
@@ -21,14 +17,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticeTheme {
-                DepositApp()
+                DepositApp(onExit = this::finish)
             }
         }
     }
 }
 
 @Composable
-fun DepositApp() {
+fun DepositApp(onExit: () -> Unit) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = androidx.compose.material3.MaterialTheme.colorScheme.background
@@ -40,9 +36,7 @@ fun DepositApp() {
             onHistoryClick = {
                 //TODO  реализовать открытие списка сохранённых расчётов
             },
-            onExitClick = {
-                //TODO  реализовать выход из приложения
-            }
+            onExitClick = onExit
         )
     }
 }
