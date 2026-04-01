b316408f0e1842e8713f19c9bae3206389ca9d94
alp80 <kyrovad014@mail.ru>
2026-04-01 13:22:20 +0700

    Добавил начальный код первого варианта

---
+++ main/src/main/java/ci/nsu/moble/main/ui/main/MainViewModel.kt
@@ -0,0 +1,35 @@
+package ci.nsu.moble.main.ui.main
+
+import androidx.lifecycle.ViewModel
+
+// UiState - простой data class
+data class CounterUiState(
+    val count: Int = 0,
+    val history: List<String> = emptyList()
+)
+
+class CounterViewModel : ViewModel() {
+    // StateFlow для UiState
+    private val _uiState = MutableStateFlow(CounterUiState())
+    val uiState: StateFlow<CounterUiState> = _uiState.asStateFlow()
+
+    // Методы для изменения состояния
+    fun increment() {
+        _uiState.update { currentState ->
+            val newCount = currentState.count + 1
+            val newHistory = listOf("+1 (итого: $newCount)") + currentState.history.take(4)
+            currentState.copy(
+                count = newCount,
+                history = newHistory
+            )
+        }
+    }
+
+    fun decrement() {
+        // TODO: реализовать аналогично increment()
+    }
+
+    fun reset() {
+        // TODO: реализовать
+    }
+}
