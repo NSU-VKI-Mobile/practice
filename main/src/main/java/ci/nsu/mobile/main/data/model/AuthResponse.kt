1ecbca3e9549b30987df9cd36105b43a081f02f9
yemeal <emelyalex@bk.ru>
2026-04-01 21:44:09 +0700

    feat: migrate network layer, auth models and repository from task_6

---
+++ main/src/main/java/ci/nsu/mobile/main/data/model/AuthResponse.kt
@@ -0,0 +1,9 @@
+package ci.nsu.mobile.main.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AuthResponse(
+    val token: String
+)
