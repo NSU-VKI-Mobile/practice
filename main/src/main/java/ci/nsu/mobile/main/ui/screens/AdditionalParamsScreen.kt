656f14e15c760d552eac623d73ec3dda49214f10
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-04-02 07:18:15 +0700

    Принимаем ставку и пополнение, инициализируем переменные для состояния полей

--- main/src/main/java/ci/nsu/mobile/main/ui/screens/AdditionalParamsScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/ui/screens/AdditionalParamsScreen.kt
@@ -7,8 +7,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,8 +23,12 @@ import androidx.compose.ui.unit.sp
 fun AdditionalParamsScreen(
     periodMonths: String,
     onBackClick: () -> Unit,
-    onCalculateClick: () -> Unit
+    onCalculateClick: (Double, String) -> Unit
 ) {
+    var monthlyTopUp = remember { mutableStateOf("") }
+    var selectedRate = remember { mutableStateOf<Double?>(null) }
+    var expanded = remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
