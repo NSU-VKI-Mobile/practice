9b28f46bc2a5fa609ad539c9dfe15ba1fedc6387
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-04-02 07:02:09 +0700

    добавляем в viewmodel хранение для данных нового экрана

--- main/src/main/java/ci/nsu/mobile/main/viewmodel/DepositViewModel.kt
+++ main/src/main/java/ci/nsu/mobile/main/viewmodel/DepositViewModel.kt
@@ -13,11 +13,26 @@ class DepositViewModel : ViewModel() {
     private val _periodMonths = MutableStateFlow("")
     val periodMonths: StateFlow<String> = _periodMonths.asStateFlow()
 
+
+    private val _interestRate = MutableStateFlow<Double?>(null)
+    val interestRate: StateFlow<Double?> = _interestRate.asStateFlow()
+
+    private val _monthlyTopUp = MutableStateFlow<String>("")
+    val monthlyTopUp: StateFlow<String> = _monthlyTopUp.asStateFlow()
+
+
     fun saveFirstScreenData(amount: String, months: String) {
         _initialAmount.value = amount
         _periodMonths.value = months
     }
 
+    fun saveSecondScreenData(rate: Double, topUp: String) {
+        _interestRate.value = rate
+        _monthlyTopUp.value = topUp
+    }
+
     fun getInitialAmount(): String = _initialAmount.value
     fun getPeriodMonths(): String = _periodMonths.value
+    fun getInterestRate(): Double? = _interestRate.value
+    fun getMonthlyTopUp(): String = _monthlyTopUp.value
 }
