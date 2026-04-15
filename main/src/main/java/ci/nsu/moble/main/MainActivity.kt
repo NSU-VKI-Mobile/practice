3487204a313fcf8b7113f686d69b94ccb23d292b
guselnikovva <guselnikovva@mer.ci.nsu.ru>
2026-04-15 14:06:50 +0700

    MainActivity.kt

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -1,13 +1,16 @@
 package ci.nsu.moble.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -47,10 +50,17 @@ fun MainScreenActivity(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // TODO:  нужно добавить  TextField
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Enter text to pass") },
+            modifier = Modifier.fillMaxWidth()
+        )
         Button(
             onClick = {
-                // TODO:  нужно добавить кнопку которая по клику открывает второе активити через интент
+                val intent = Intent(context, SecondActivity::class.java)
+                intent.putExtra("text_data", text)
+                context.startActivity(intent)
             },
             modifier = Modifier.padding(top = 16.dp)
         ) {
