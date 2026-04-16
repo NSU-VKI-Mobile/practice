cb13769e5cf05e2349abaedbfa4e7abbceca630a
BogatyryovaUI <bogatyryovaui@mer.ci.nsu.ru>
2026-04-16 10:35:48 +0700

    1223

---
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -0,0 +1,19 @@
+package ci.nsu.moble.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import ci.nsu.moble.main.ui.theme.PracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeTheme {
+                MyScreen()
+            }
+        }
+    }
+}
