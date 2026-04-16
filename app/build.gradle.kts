ed7adc2aacb8e4c71ec4dc6efe3fa4cd97e8fec0
VysotovAE <VysotovAE@mer.ci.nsu.ru>
2026-04-16 12:05:49 +0700

    добавлены dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,13 @@ android {
 }
 
 dependencies {
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
