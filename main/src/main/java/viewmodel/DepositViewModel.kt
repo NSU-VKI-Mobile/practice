4dbb3956a8f785ce7a7cc6d7a894006e479c1329
Goroshek <goroshek@example.com>
2026-04-22 02:20:44 +0700

    viewModel: переменные, функции основные

---
+++ main/src/main/java/viewmodel/DepositViewModel.kt
@@ -0,0 +1,93 @@
+package viewmodel
+
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import data.DepositCalculations
+import data.DepositRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import java.time.Month
+import java.time.temporal.TemporalAmount
+
+class DepositViewModel(
+    private val repository: DepositRepository
+) : ViewModel() {
+
+    private val _termInMonths = MutableStateFlow("") //срок в месяцах
+    private val _interestRate = MutableStateFlow(0.0) //ставка
+    private val _monthlyDeposit = MutableStateFlow("") //ежемесячный платеж
+    private val _selectedCurrency = MutableStateFlow("Рубли") //выбранная валюта
+    private val _initialAmount = MutableStateFlow("") //начальная сумма
+    private val _totalAmount = MutableStateFlow(0.0) //итоговая сумма
+    private val _accruedInterest = MutableStateFlow(0.0) //начисленные проценты
+    private val _errorMessage = MutableStateFlow(null) //ошибка
+
+
+    val termInMonths: StateFlow<String> = _termInMonths.asStateFlow()
+    val interestRate: StateFlow<Double> = _interestRate.asStateFlow()
+    val monthlyDeposit: StateFlow<String> = _monthlyDeposit.asStateFlow()
+    val selectedCurrency: StateFlow<String> = _selectedCurrency.asStateFlow()
+    val initialAmount: StateFlow<String> = _initialAmount.asStateFlow()
+    val totalAmount: StateFlow<Double> = _totalAmount.asStateFlow()
+    val accruedInterest: StateFlow<Double> = _accruedInterest.asStateFlow()
+    val errorMessage: StateFlow<String?> = _errorMessage.asStateFlow()
+
+
+    //обновление начальной суммы
+    fun updateInititalAmount(value: String) {
+        _initialAmount.value = value
+    }
+    //обновление срока в месяцах
+    fun updateTermInMonths(value: String) {
+        _termInMonths.value = value
+        val months = value.toIntOrNull() ?: 0
+        _interestRate.value = calculateInterestRate(months)
+    }
+    //обновление ежемесячного платежа
+    fun updateMonthlyDeposit(value: String) {
+        _monthlyDeposit.value = value
+    }
+    //обновление выбранной валюты
+    fun updateSelectedCurrency(value: String) {
+        _selectedCurrency.value = value
+    }
+
+    //расчет процентной ставки по сроку
+    private fun calculateInterestRate(months: Int): Double {
+        return when {
+            months < 6-> 15.0
+            months < 12 -> 10.0
+            else -> 5.0
+        }
+    }
+
+    //расчет итоговой суммы
+    private fun calculateTotalAmount(amount: Double, term: Int, deposit: Double) :
+            Pair<Double, Double> //возврат 2-х значений
+    {
+        val rate = calculateInterestRate(term)
+        val monthlyRate = rate / 12.0 / 100.0
+        var currentAmount = amount
+        for (n in 1..term)
+        {
+            currentAmount += (currentAmount * monthlyRate)
+            currentAmount += deposit
+        }
+        val totalDeposited = amount + (deposit * term)
+        val interestEarned = currentAmount - totalDeposited
+        return Pair(currentAmount, interestEarned)
+    }
+
+
+    //TODO:сохранение расчета
+
+
+    //загрузка истории
+    fun getAllHistory(): Flow<List<DepositCalculations>> {
+        return repository.getAllCalculations()
+    }
+
+
+}
