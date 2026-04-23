40acc4b2da7cdb9356fc5481ea7d31e398173467
VV21buhtig <zubladddjs@gmail.com>
2026-04-23 10:28:45 +0700

    wswsw23ВВУ

--- main/src/main/java/ci/nsu/mobile/main/data/remote/dto/PersonDto.kt
+++ main/src/main/java/ci/nsu/mobile/main/data/remote/dto/PersonDto.kt
@@ -3,6 +3,7 @@ package ci.nsu.mobile.main.data.remote.dto
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 data class PersonDto(
     @SerialName("firstName")
