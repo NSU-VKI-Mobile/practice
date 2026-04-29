8e28b687af977256a571ecb905232d16385cebca
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-04-30 05:22:55 +0700

    Создание ApiService (интерфейс для запросов???)

---
+++ main/src/main/java/ci/nsu/mobile/auth/data/network/ApiService.kt
@@ -0,0 +1,23 @@
+package ci.nsu.mobile.auth.data.network
+
+import ci.nsu.mobile.auth.data.models.*
+import retrofit2.http.*
+
+interface ApiService {
+
+    @POST("auth/register")
+    suspend fun register(
+        @Body request: RegisterRequest
+    ): retrofit2.Response<Unit>
+
+    @POST("auth/login")
+    suspend fun login(
+        @Body request: LoginRequest
+    ): retrofit2.Response<AuthResponse>
+
+    @GET("groups")
+    suspend fun getGroups(): retrofit2.Response<List<GroupDto>>
+
+    @GET("users")
+    suspend fun getUsers(): retrofit2.Response<List<UserDto>>
+}
