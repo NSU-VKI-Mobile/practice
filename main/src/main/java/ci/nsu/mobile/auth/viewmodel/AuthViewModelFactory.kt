6a872e904fbd9ed966d2b175eb1f579e02b2cd32
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-04-30 05:47:20 +0700

    Создали фабрику, которая принимает AuthRepository в конструкторе Когда Compose запрашивает AuthViewModel, фабрика создаёт его с нужным репозиторием

---
+++ main/src/main/java/ci/nsu/mobile/auth/viewmodel/AuthViewModelFactory.kt
@@ -0,0 +1,18 @@
+package ci.nsu.mobile.auth.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ci.nsu.mobile.auth.data.repository.AuthRepository
+
+class AuthViewModelFactory(
+    private val repository: AuthRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AuthViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AuthViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
