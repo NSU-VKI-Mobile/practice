ac9d2573410af4322dc6f79f131315bbd566a4b4
goddamngh <saveliishiriev@gmail.com>
2026-04-30 11:51:59 +0700

    feat: connect all screens to NavHost in MainActivity

--- main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -3,18 +3,19 @@ package ci.nsu.mobile.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import ci.nsu.mobile.main.screens.HistoryScreen
+import ci.nsu.mobile.main.screens.MainScreen
+import ci.nsu.mobile.main.screens.ResultScreen
+import ci.nsu.mobile.main.screens.Step1Screen
+import ci.nsu.mobile.main.screens.Step2Screen
 import ci.nsu.mobile.main.ui.theme.PracticeTheme
 import ci.nsu.mobile.main.viewmodel.DepositViewModel
 
@@ -23,19 +24,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticeTheme {
-                val navController = rememberNavController()
-                val viewModel: DepositViewModel = viewModel()
-
-                NavHost(
-                    navController = navController,
-                    startDestination = "main" // Начинаем с главного экрана
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    // Описываем маршруты (экраны создадим в следующем шаге)
-                    composable("main") { /* Тут будет MainScreen(navController) */ }
-                    composable("step1") { /* Тут будет Step1Screen(navController, viewModel) */ }
-                    composable("step2") { /* Тут будет Step2Screen(navController, viewModel) */ }
-                    composable("result") { /* Тут будет ResultScreen(navController, viewModel) */ }
-                    composable("history") { /* Тут будет HistoryScreen(navController, viewModel) */ }
+                    val navController = rememberNavController()
+                    val viewModel: DepositViewModel = viewModel()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "main"
+                    ) {
+                        composable("main") {
+                            MainScreen(navController)
+                        }
+                        composable("step1") {
+                            Step1Screen(navController, viewModel)
+                        }
+                        composable("step2") {
+                            Step2Screen(navController, viewModel)
+                        }
+                        composable("result") {
+                            ResultScreen(navController, viewModel)
+                        }
+                        composable("history") {
+                            HistoryScreen(navController, viewModel)
+                        }
+                    }
                 }
             }
         }
