7b75c7a7bc99003b49e3a2e521e832bcea47e7cf
GrohotovDE <grohotovde@mer.ci.nsu>
2026-04-30 10:22:02 +0700

    ...h

---
+++ main/src/main/java/ci/nsu/mobile/main/ui/Model.kt
@@ -0,0 +1,12 @@
+package ci.nsu.mobile.main.ui
+
+data class ShoppingItem(
+    val id: Int,
+    val name: String,
+    val isBought: Boolean = false
+)
+
+data class ShoppingListUiState(
+    val items: List<ShoppingItem> = emptyList(),
+    val newItemText: String = ""
+)
