d6a39df5de1485278eece54bcf43c25b6df5bea8
voxzair <zairjack@gmail.com>
2026-05-04 23:50:52 +0700

    Исправил UI

--- main/src/main/java/ci/nsu/mobile/main/ui/main/TemperatureScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/ui/main/TemperatureScreen.kt
@@ -3,6 +3,7 @@ package ci.nsu.mobile.main.ui.main
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -19,7 +20,8 @@ fun TemperatureScreenContent(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         OutlinedTextField(
@@ -27,23 +29,33 @@ fun TemperatureScreenContent(
             onValueChange = viewModel::onCelsiusChanged,
             label = { Text("Цельсий") },
             isError = uiState.celsius.isNotBlank() && !uiState.isCelsiusValid,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(0.8f)
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         OutlinedTextField(
             value = uiState.fahrenheit,
             onValueChange = viewModel::onFahrenheitChanged,
             label = { Text("Фаренгейт") },
             isError = uiState.fahrenheit.isNotBlank() && !uiState.isFahrenheitValid,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(0.8f)
         )
 
+        Spacer(modifier = Modifier.height(16.dp))
+
         if (uiState.celsius.isNotBlank() && !uiState.isCelsiusValid) {
-            Text("Ошибка ввода Цельсий", color = Color.Red)
+            Text(
+                text = "Ошибка ввода Цельсий",
+                color = Color.Red
+            )
         }
 
         if (uiState.fahrenheit.isNotBlank() && !uiState.isFahrenheitValid) {
-            Text("Ошибка ввода Фаренгейт", color = Color.Red)
+            Text(
+                text = "Ошибка ввода Фаренгейт",
+                color = Color.Red
+            )
         }
     }
 }
