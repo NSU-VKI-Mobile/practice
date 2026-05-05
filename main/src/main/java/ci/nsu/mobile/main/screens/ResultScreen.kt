fd07f586f0d50703b9844ffac913641c0064f9ee
voxzair <zairjack@gmail.com>
2026-05-05 23:50:35 +0700

    базовая навигация

---
+++ main/src/main/java/ci/nsu/mobile/main/screens/ResultScreen.kt
@@ -0,0 +1,12 @@
+package ci.nsu.mobile.main.screens
+
+package ci.nsu.mobile.main.screens
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ResultScreen(navController: NavController) {
+    Text("Result")
+}
