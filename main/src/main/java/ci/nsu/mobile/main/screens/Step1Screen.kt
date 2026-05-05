189beb9b4f30ba3ac45c1883273bbf683781cdc1
voxzair <zairjack@gmail.com>
2026-05-06 00:30:02 +0700

    Step1Screen и Step2Screen

--- main/src/main/java/ci/nsu/mobile/main/screens/Step1Screen.kt
+++ main/src/main/java/ci/nsu/mobile/main/screens/Step1Screen.kt
@@ -1,10 +1,42 @@
 package ci.nsu.mobile.main.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import ci.nsu.mobile.main.ui.viewmodel.DepositViewModel
 
 @Composable
-fun Step1Screen(navController: NavController) {
-    Text("Step 1")
+fun Step1Screen(navController: NavController, vm: DepositViewModel = viewModel()) {
+
+    val amount by vm.initialAmount.collectAsState()
+    val months by vm.months.collectAsState()
+
+    Column {
+        TextField(
+            value = amount,
+            onValueChange = vm::setInitialAmount,
+            label = { Text("Стартовый взнос") }
+        )
+
+        TextField(
+            value = months,
+            onValueChange = vm::setMonths,
+            label = { Text("Срок (месяцы)") }
+        )
+
+        Button(onClick = { navController.navigate("main") }) {
+            Text("В начало")
+        }
+
+        Button(onClick = {
+            vm.calculateRate()
+            navController.navigate("step2")
+        }) {
+            Text("Далее")
+        }
+    }
 }
--- main/src/main/java/ci/nsu/mobile/main/screens/Step2Screen.kt
+++ main/src/main/java/ci/nsu/mobile/main/screens/Step2Screen.kt
@@ -1,10 +1,37 @@
 package ci.nsu.mobile.main.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import ci.nsu.mobile.main.ui.viewmodel.DepositViewModel
 
 @Composable
-fun Step2Screen(navController: NavController) {
-    Text("Step 2")
+fun Step2Screen(navController: NavController, vm: DepositViewModel = viewModel()) {
+
+    val rate by vm.rate.collectAsState()
+    val topUp by vm.topUp.collectAsState()
+
+    Column {
+        Text("Ставка: $rate %")
+
+        TextField(
+            value = topUp,
+            onValueChange = vm::setTopUp,
+            label = { Text("Пополнение") }
+        )
+
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Назад")
+        }
+
+        Button(onClick = {
+            navController.navigate("result")
+        }) {
+            Text("Рассчитать")
+        }
+    }
 }
