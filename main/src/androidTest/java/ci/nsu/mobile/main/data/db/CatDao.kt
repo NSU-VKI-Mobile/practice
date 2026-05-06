1f2b8d6b0a4e223cc8a92ab3884837cd37551285
adamqqaa <adamqwertyadam11@gmail.com>
2026-05-06 21:19:44 +0700

    БД 2

--- main/src/androidTest/java/ci/nsu/mobile/main/data/db/CatDao.kt
+++ main/src/androidTest/java/ci/nsu/mobile/main/data/db/CatDao.kt
@@ -1,4 +1,19 @@
 package ci.nsu.mobile.main.data.db
 
-class CatDao {
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CatDao {
+    @Query("SELECT * FROM cats ORDER BY dateAdded DESC")
+    fun getAllCatsFlow(): Flow<List<CatEntity>>
+
+    @Insert
+    suspend fun insertCat(cat: CatEntity): Long
+
+    @Delete
+    suspend fun deleteCat(cat: CatEntity)
+
+    @Query("DELETE FROM cats WHERE id IN (:ids)")
+    suspend fun deleteCatsByIds(ids: List<Long>)
 }
