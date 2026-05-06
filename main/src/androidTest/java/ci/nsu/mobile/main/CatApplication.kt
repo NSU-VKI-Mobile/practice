b5556bb37f7a056f1e90b5036816ac49a49ebe7d
adamqqaa <adamqwertyadam11@gmail.com>
2026-05-06 21:17:41 +0700

    Я случайно пропустил переименование коммитов

--- main/src/androidTest/java/ci/nsu/mobile/main/CatApplication.kt
+++ main/src/androidTest/java/ci/nsu/mobile/main/CatApplication.kt
@@ -1,4 +1,10 @@
 package ci.nsu.mobile.main
 
-class CatApplication {
+import android.app.Application
+import ci.nsu.mobile.main.data.db.CatDatabase
+import ci.nsu.mobile.main.data.repository.CatRepository
+
+class CatApplication : Application() {
+    val database by lazy { CatDatabase.getDatabase(this) }
+    val repository by lazy { CatRepository(database.catDao()) }
 }
