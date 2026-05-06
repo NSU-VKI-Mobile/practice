fbf54186c3d7c68069ad73d7cdd645f7a553c220
voxzair <zairjack@gmail.com>
2026-05-07 00:19:58 +0700

    Добавлена валидация

--- main/src/main/java/ci/nsu/mobile/main/screens/Step1Screen.kt
+++ main/src/main/java/ci/nsu/mobile/main/screens/Step1Screen.kt
@@ -1,26 +1,35 @@
-package ci.nsu.mobile.main.screens
+package ci.nsu.mobile.main.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import ci.nsu.mobile.main.ui.viewmodel.DepositViewModel
 
 @Composable
-fun Step1Screen(navController: NavController, vm: DepositViewModel = viewModel()) {
+fun Step1Screen(navController: NavController, vm: DepositViewModel) {
 
     val amount by vm.initialAmount.collectAsState()
     val months by vm.months.collectAsState()
 
-    Column {
+    var error by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
         TextField(
             value = amount,
             onValueChange = vm::setInitialAmount,
             label = { Text("Стартовый взнос") }
         )
+
+        Spacer(modifier = Modifier.height(10.dp))
 
         TextField(
             value = months,
@@ -28,13 +37,25 @@ fun Step1Screen(navController: NavController, vm: DepositViewModel = viewModel()
             label = { Text("Срок (месяцы)") }
         )
 
+        if (error.isNotEmpty()) {
+            Text(error, color = MaterialTheme.colorScheme.error)
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
         Button(onClick = { navController.navigate("main") }) {
             Text("В начало")
         }
 
+        Spacer(modifier = Modifier.height(10.dp))
+
         Button(onClick = {
-            vm.calculateRate()
-            navController.navigate("step2")
+            if (amount.isEmpty() || months.isEmpty()) {
+                error = "Заполните все поля"
+            } else {
+                vm.calculateRate()
+                navController.navigate("step2")
+            }
         }) {
             Text("Далее")
         }
