b0139b09a1faea4c6ac1e225ff9d29ae7abc2a30
adamqqaa <adamqwertyadam11@gmail.com>
2026-05-06 21:18:46 +0700

    Репозиторий

--- main/src/androidTest/java/ci/nsu/mobile/main/data/repository/CatRepository.kt
+++ main/src/androidTest/java/ci/nsu/mobile/main/data/repository/CatRepository.kt
@@ -1,4 +1,15 @@
 package ci.nsu.mobile.main.data.repository
 
-class CatRepository {
+import ci.nsu.mobile.main.data.db.CatDao
+import ci.nsu.mobile.main.data.db.CatEntity
+import kotlinx.coroutines.flow.Flow
+
+class CatRepository(private val catDao: CatDao) {
+    fun getAllCatsFlow(): Flow<List<CatEntity>> = catDao.getAllCatsFlow()
+
+    suspend fun insertCat(cat: CatEntity): Long = catDao.insertCat(cat)
+
+    suspend fun deleteCat(cat: CatEntity) = catDao.deleteCat(cat)
+
+    suspend fun deleteCatsByIds(ids: List<Long>) = catDao.deleteCatsByIds(ids)
 }
