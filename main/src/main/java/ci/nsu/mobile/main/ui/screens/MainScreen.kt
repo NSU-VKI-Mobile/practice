ef7dd8e1e95566979090344fb5552df202e81e77
BykovAM <bykovam@mer.ci.nsu.ru>
2026-05-06 11:50:22 +0700

    fix button in main

--- main/src/main/java/ci/nsu/mobile/main/ui/screens/MainScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/ui/screens/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -50,7 +51,13 @@ fun MainScreen(
                 }
             }
         }
-        Button(onClick = onLogOutClick) {
+        Button(
+            onClick = onLogOutClick,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
             Text(stringResource(R.string.text_logOut))
         }
     }
