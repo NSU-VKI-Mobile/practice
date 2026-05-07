1a9b4d032e3cf4007692d6f91250c671be2553d0
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2026-05-07 14:58:18 +0700

    push

--- main/src/main/java/ci/nsu/mobile/main/data/network/NetworkModule.kt
+++ main/src/main/java/ci/nsu/mobile/main/data/network/NetworkModule.kt
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object NetworkModule {
 
+    //пупупууу
     private const val BASE_URL = "http://192.168.200.160:8080/api/"
 //    private const val BASE_URL = "http://192.168.1.218:8080/api/"
 //    private const val BASE_URL = "http://10.0.2.2:8080/api/"
