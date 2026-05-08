88d518b68b9f97ec0297e2a5a5536429d95f1c33
KorolevaVE <Korolevave@mer.ci.nsu.ru>
2026-05-08 12:11:32 +0700

    finish task 4

--- main/src/androidTest/java/ci/nsu/mobile/main/ExampleInstrumentedTest.kt
+++ main/src/androidTest/java/ci/nsu/mobile/main/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ci.nsu.mobile.main", appContext.packageName)
+        assertEquals("ci.nsu.mobile.main", appContext.packageName);
     }
 }
