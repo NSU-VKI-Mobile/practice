f8593ce9b0cfcedfb8156e468c39818f67e5aeaf
Anna <prokudinaanna2006.ap@gmail.com>
2026-05-11 16:49:35 +0700

    update data transfer objects

--- main/src/main/java/ci/nsu/mobile/main/data/network/model/UserDto.kt
+++ main/src/main/java/ci/nsu/mobile/main/data/network/model/UserDto.kt
@@ -5,9 +5,12 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class UserDto(
     val userId: Int,
-    val person: PersonDto,
     val login: String,
     val email: String,
-    val phoneNumber: String,
-    val roleId: Int = 1
+    val phoneNumber: String?,
+    val roleId: Int = 1,
+    val authAllowed: Boolean,
+    val personId: Int,
+    val createdDate: String,
+    val lastLoginDate: String?,
 )
