65ea774bfd0b7c758be30514d906fc19c0ab3d00
RedPen <bykovam@mer.ci.nsu.ru>
2026-05-12 18:07:40 +0700

    IN PROGRESS

--- main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -18,29 +18,34 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import ci.nsu.mobile.main.data.repository.DepositRepository
 import ci.nsu.mobile.main.sl.ServiceLocator
 import ci.nsu.mobile.main.ui.screens.CalcScreen
+import ci.nsu.mobile.main.ui.screens.ErrorScreen
 import ci.nsu.mobile.main.ui.screens.HistoryCalcScreen
 import ci.nsu.mobile.main.ui.screens.Input1Screen
 import ci.nsu.mobile.main.ui.screens.Input2Screen
-import ci.nsu.mobile.main.ui.screens.MainScreen
+import ci.nsu.mobile.main.ui.screens.LogInScreen
+import ci.nsu.mobile.main.ui.screens.RegistryScreen
 import ci.nsu.mobile.main.ui.theme.PracticeTheme
 import ci.nsu.mobile.main.vm.DepositsViewModel
 import ci.nsu.mobile.main.vm.LoginAndRegViewModel
 
 sealed class Screen(val route: String) {
+    object LogIn : Screen("login")
+    object Registry : Screen("registry")
     object Main : Screen("main")
     object Input1 : Screen("input1")
     object Input2 : Screen("input2")
     object Calc : Screen("calc")
     object HistoryCalc : Screen("HistoryCalc")
-    object LogIn : Screen("login")
-    object Registry : Screen("registry")
-    object Main2 : Screen("main")
 }
 
 class MainActivity : ComponentActivity() {
@@ -65,7 +70,6 @@ fun Greeting(
 ) {
     val context = LocalContext.current
     val serviceLocator = remember { ServiceLocator.getInstance(context) }
-    var selectedTab by remember { mutableIntStateOf(0) }
 
     val authViewModel: LoginAndRegViewModel = viewModel(
         factory = serviceLocator.viewModelFactory
@@ -78,48 +82,49 @@ fun Greeting(
     val token by authViewModel.token.collectAsState()
     val navController = rememberNavController()
     NavHost(
-        modifier = modifier,
+        modifier = modifier
+            .padding(start = 20.dp),
         navController = navController,
-        startDestination = Screen.Main.route
+        startDestination = Screen.LogIn.route
     ) {
+        composable(Screen.LogIn.route) {
+            LogInScreen(
+                onRegClick = {navController.navigate(Screen.Registry.route) },
+                onLogInClick = { authViewModel.logIn({navController.navigate(Screen.Registry.route)})},
+                onExitClick = {(context as? Activity)?.finish()},
+                viewModel = authViewModel
+            )
+        }
+
+        composable(Screen.Registry.route) {
+            RegistryScreen(
+                onRegClick = {
+                    authViewModel.registry()
+                    navController.navigate(Screen.LogIn.route) },
+                onBackClick = {navController.popBackStack()},
+                viewModel = authViewModel
+            )
+        }
+
         composable(Screen.Main.route) {
             MainScreen(
-                onCalculateClick = { navController.navigate(Screen.Input1.route) },
-                onHistoryClick = { navController.navigate(Screen.HistoryCalc.route) },
-                onCloseClick = {(context as? Activity)?.finish()}
-            )
-        }
-
-        composable(Screen.Input1.route) {
-            Input1Screen(
-                onBackClick = { navController.popBackStack(Screen.Main.route, inclusive = false) },
-                onNextClick = {navController.navigate(Screen.Input2.route) },
-                viewModel = depositViewModel
-            )
-        }
-
-        composable(Screen.Input2.route) {
-            Input2Screen(
-                onBackClick = { navController.popBackStack() },
-                onCalcClick = { navController.navigate(Screen.Calc.route) },
-                viewModel = depositViewModel
-            )
-        }
-
-        composable(Screen.Calc.route) {
-            CalcScreen(
-                onMainClick = { navController.popBackStack(Screen.Main.route, inclusive = false) },
-                viewModel = depositViewModel
-            )
-        }
-
-        composable(Screen.HistoryCalc.route) {
-            HistoryCalcScreen(
-                onBackClick = { navController.popBackStack()}
+                onLogOutClick = {
+                    authViewModel.logOut()
+                    navController.navigate(Screen.LogIn.route)
+                },
+                viewModel = authViewModel
             )
         }
     }
+
+    authViewModel.errorMessage?.let{e ->
+        ErrorScreen(
+            onDismiss = {authViewModel.errorMessage = null},
+            onExit = {(context as? Activity)?.finish()},
+            error = e)
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
--- main/src/main/java/ci/nsu/mobile/main/ui/screens/MainScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/ui/screens/MainScreen.kt
@@ -1,26 +1,24 @@
 package ci.nsu.mobile.main.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import ci.nsu.mobile.main.vm.DepositsViewModel
+import ci.nsu.mobile.main.vm.LoginAndRegViewModel
+
+sealed class Screen(val route: String) {
+    object Input1 : Screen("input1")
+    object Input2 : Screen("input2")
+    object Calc : Screen("calc")
+    object HistoryCalc : Screen("historyCalc")
+    object UserList: Screen("userList")
+}
+
 
 @Composable
 fun MainScreen(
-    onCalculateClick: () -> Unit,
-    onHistoryClick: () -> Unit,
-    onCloseClick: () -> Unit
-) {
-    Column() {
-        Text("Расчёт вкладов")
-        Button(onClick = onCalculateClick) {
-            Text("Рассчитать")
-        }
-        Button(onClick = onHistoryClick) {
-            Text("История расчётов")
-        }
-        Button(onClick = onCloseClick) {
-            Text("Закрыть приложение")
-        }
-    }
+    navController: NavController,
+    depositsViewModel: DepositsViewModel,
+    authViewModel: LoginAndRegViewModel
+){
+
 }
