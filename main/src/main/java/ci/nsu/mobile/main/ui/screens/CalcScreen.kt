8fc13807cd82b28c763879ec3fe47b4e6648f9eb
RedPen <bykovam@mer.ci.nsu.ru>
2026-05-12 22:32:29 +0700

    fix code

--- main/src/main/java/ci/nsu/mobile/main/ui/screens/CalcScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/ui/screens/CalcScreen.kt
@@ -2,7 +2,6 @@ package ci.nsu.mobile.main.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
