63e9ceff0074787226dc115177257e4d883453c7
Anna <prokudinaanna2006.ap@gmail.com>
2026-05-17 23:49:07 +0700

    realized users state, viewModel and screen

--- main/src/main/java/ci/nsu/mobile/main/ui/screens/UsersScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/ui/screens/UsersScreen.kt
@@ -1,30 +1,70 @@
-package ci.nsu.mobile.main.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import ci.nsu.mobile.main.navigation.Screens
 import ci.nsu.mobile.main.ui.components.CustomButton
+import ci.nsu.mobile.main.viewmodel.UsersViewModel
 
 @Composable
-fun UsersScreen(navTo: (String) -> Unit) {
-    Scaffold() { innerPadding ->
+fun UsersScreen(
+    navTo: (String) -> Unit,
+    viewModel: UsersViewModel
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.loadUsers()
+    }
+
+
+    Scaffold { innerPadding ->
         Column(
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            modifier = Modifier.fillMaxSize()
+                .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            CustomButton({ navTo(Screens.LoginScreen.route) }, "Выйти")
+            if (state.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.padding(20.dp))
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .padding(horizontal = 10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                items(state.users) { user ->
+                    UserCard(user)
+                }
+
+            }
+
+            if (state.errorMessage != null) {
+                Text(
+                    state.errorMessage.toString(),
+                    color = Color.Red,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+
+            CustomButton(
+                onClick = { navTo(Screens.LoginScreen.route) }, title = "Выйти"
+            )
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewUsers() {
-    UsersScreen({})
 }
---
+++ main/src/main/java/ci/nsu/mobile/main/viewmodel/UsersViewModel.kt
@@ -0,0 +1,39 @@
+package ci.nsu.mobile.main.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ci.nsu.mobile.main.data.network.TokenManager
+import ci.nsu.mobile.main.data.repository.AuthRepository
+import ci.nsu.mobile.main.viewmodel.state.UserState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UsersViewModel @Inject constructor(
+    private val repository: AuthRepository,
+    private val tokenManager: TokenManager
+) : ViewModel() {
+    private val _state = MutableStateFlow(UserState())
+    val state: StateFlow<UserState> = _state.asStateFlow()
+
+    init {
+        loadUsers()
+    }
+
+    fun loadUsers() {
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+            val result = repository.getUsers()
+            result.onSuccess { users ->
+                _state.update { it.copy(users = users, isLoading = false, errorMessage = null) }
+            }.onFailure { error ->
+                _state.update { it.copy(isLoading = false, errorMessage = "Ошибка загрузки пользователей: ${error.message}") }
+            }
+        }
+    }
+}
