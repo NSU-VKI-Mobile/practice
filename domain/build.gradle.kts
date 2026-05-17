49a809e2e316c4fa65ff0ccdcac007a0b65ec485
RedPen <bykovam@mer.ci.nsu.ru>
2026-05-17 15:50:54 +0700

    add modules, finish domain

---
+++ domain/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "ci.nsu.mobile.domain"
+    compileSdk = 36
+
+    defaultConfig { minSdk = 24 }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+        }
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
+}
