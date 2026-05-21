03d009c1cc077e66f77e2c1e3e912ae7b8a71e0c
GolovinKM <golovinkm@mer.ci.nsu.ru>
2026-05-21 11:23:22 +0700

    Создаём пустой MainActivity.kt(просто базовый экран)

---
+++ main/src/main/java/ci/nsu/mobile/main/ui/MainScreen.kt
@@ -0,0 +1,13 @@
+package ci.nsu.mobile.main.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello gavlou!",
+        modifier = modifier
+    )
+}
