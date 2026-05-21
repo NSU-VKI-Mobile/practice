2c3eb5672b710d2aff91a205b233d21414cc08b8
MaksimovskayaTE <maksimovskayate@mer.ci.nsu.ru>
2026-05-21 10:59:56 +0700

    ae;lhib;fneb

--- main/src/main/java/ci/nsu/mobile/main/units/data/interceptor/AuthInterceptor.kt
+++ main/src/main/java/ci/nsu/mobile/main/units/data/interceptor/AuthInterceptor.kt
@@ -3,15 +3,20 @@ package ci.nsu.mobile.main.units.data.interceptor
 import ci.nsu.mobile.main.units.data.token.TokenManager
 import okhttp3.Interceptor
 import okhttp3.Response
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class AuthInterceptor(private val tokenManager: TokenManager): Interceptor {
+@Singleton
+class AuthInterceptor @Inject constructor(
+    private val tokenManager: TokenManager
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val original = chain.request()
-        val builder = original.newBuilder()
+        val originalRequest = chain.request()
+        val requestBuilder = originalRequest.newBuilder()
             .addHeader("Content-Type", "application/json")
-        tokenManager.token?.let { token ->
-            builder.addHeader("Authorization", "Bearer $token")
+        tokenManager.token?.let {
+            requestBuilder.addHeader("Authorization", "Bearer $it")
         }
-        return chain.proceed(builder.build())
+        return chain.proceed(requestBuilder.build())
     }
 }
