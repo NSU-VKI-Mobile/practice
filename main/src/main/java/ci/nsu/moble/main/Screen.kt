55b61628ff900f2186afd24974a854e1fe621be9
Ершов Владислав Сергеевич <ErshovVS@dom201.local>
2026-05-21 15:59:48 +0700

    ErshovVS_2307b_task3

---
+++ main/src/main/java/ci/nsu/moble/main/Screen.kt
@@ -0,0 +1,10 @@
+package ci.nsu.moble.main
+sealed class Screen(val route: String) {
+
+    object Home : Screen("home")
+
+    object Profile : Screen("profile")
+
+    object Settings : Screen("settings")
+
+}
