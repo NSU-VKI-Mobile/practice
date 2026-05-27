283deb5d71f5a92e685db4af7b3b1967312b6de7
Daniil <klochkovdy@mer.ci.nsu.ru>
2026-05-27 22:43:37 +0700

    fix

--- main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/MainActivity.kt
@@ -3,7 +3,12 @@ package ci.nsu.mobile.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -16,13 +21,18 @@ import ci.nsu.mobile.main.ui.theme.PracticeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Инициализируем TokenManager с контекстом
+        enableEdgeToEdge()
         TokenManager.init(applicationContext)
 
         setContent {
             PracticeTheme {
-                AppNavigation()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .safeDrawingPadding()
+                ) {
+                    AppNavigation()
+                }
             }
         }
     }
