53d581b9ae80836aa837382d5f7d010cd9fa9d3b
BykovAM <bykovam@mer.ci.nsu.ru>
2026-05-28 09:17:56 +0700

    fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "ci.nsu.mobile.main"
-        minSdk = 24
+        minSdk = 29
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
--- calculations/build.gradle.kts
+++ calculations/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 29
     }
 
     buildTypes {
