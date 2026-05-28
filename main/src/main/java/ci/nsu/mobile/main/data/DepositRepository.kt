7d94699f9041ec14f819e42bbbf6ea6571a11463
tapokdeda20 <104147128+tapokdeda20@users.noreply.github.com>
2026-05-28 12:38:05 +0700

    Add repository method for calculation details

--- main/src/main/java/ci/nsu/mobile/main/data/DepositRepository.kt
+++ main/src/main/java/ci/nsu/mobile/main/data/DepositRepository.kt
@@ -6,6 +6,10 @@ class DepositRepository(private val dao: DepositDao) {
 
     val calculations: Flow<List<DepositCalculation>> = dao.getAllCalculations()
 
+    fun getCalculationById(id: Long): Flow<DepositCalculation?> {
+        return dao.getCalculationById(id)
+    }
+
     suspend fun saveCalculation(calculation: DepositCalculation) {
         dao.insert(calculation)
     }
