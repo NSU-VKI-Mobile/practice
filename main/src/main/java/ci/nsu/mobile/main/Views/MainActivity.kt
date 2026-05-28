6a4dc1fd41833177baf497ce57f768b85b451c61
DobrovolskiyIV <DobrovolskiyIV@306D-11>
2026-05-28 13:56:57 +0700

    Добавил TopAppBar в RegistrationScreen.kt

--- main/src/main/java/ci/nsu/mobile/main/Views/MainActivity.kt
+++ main/src/main/java/ci/nsu/mobile/main/Views/MainActivity.kt
@@ -2,42 +2,14 @@ package ci.nsu.mobile.main.Views
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.LinkAnnotation
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextLinkStyles
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withLink
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -109,14 +81,6 @@ class MainActivity : ComponentActivity() {
     }
 
     // Вспомогательная функция для создания фабрики ViewModel
-    private inline fun <reified VM : ViewModel> savedStateViewModelFactory(
-        crossinline factory: (SavedStateHandle) -> VM
-    ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return factory(SavedStateHandle()) as T
-        }
-    }
-
     private inline fun <reified VM : ViewModel> viewModelFactory(
         crossinline factory: () -> VM
     ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
