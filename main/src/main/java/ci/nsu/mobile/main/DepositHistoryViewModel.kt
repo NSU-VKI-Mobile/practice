71cf86dceb92bb639e52632fadd6aed4095c944e
tapokdeda20 <104147128+tapokdeda20@users.noreply.github.com>
2026-06-02 11:28:45 +0700

    Add history delete actions

--- main/src/main/java/ci/nsu/mobile/main/DepositHistoryViewModel.kt
+++ main/src/main/java/ci/nsu/mobile/main/DepositHistoryViewModel.kt
@@ -1,11 +1,25 @@
 package ci.nsu.mobile.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import ci.nsu.mobile.main.data.DepositCalculation
 import ci.nsu.mobile.main.data.DepositRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
-class DepositHistoryViewModel(repository: DepositRepository) : ViewModel() {
+class DepositHistoryViewModel(private val repository: DepositRepository) : ViewModel() {
 
     val calculations: Flow<List<DepositCalculation>> = repository.calculations
+
+    fun deleteCalculation(calculation: DepositCalculation) {
+        viewModelScope.launch {
+            repository.deleteCalculationById(calculation.id)
+        }
+    }
+
+    fun clearHistory() {
+        viewModelScope.launch {
+            repository.clearAllCalculations()
+        }
+    }
 }
