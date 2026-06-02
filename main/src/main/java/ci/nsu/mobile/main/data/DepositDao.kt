1833f4f1b38cc3c27331d68302c952e7fceea37e
tapokdeda20 <104147128+tapokdeda20@users.noreply.github.com>
2026-06-02 11:20:42 +0700

    Add deleting calculations from history

--- main/src/main/java/ci/nsu/mobile/main/data/DepositDao.kt
+++ main/src/main/java/ci/nsu/mobile/main/data/DepositDao.kt
@@ -16,4 +16,10 @@ interface DepositDao {
 
     @Query("SELECT * FROM deposit_calculations WHERE id = :id")
     fun getCalculationById(id: Long): Flow<DepositCalculation?>
+
+    @Query("DELETE FROM deposit_calculations WHERE id = :id")
+    suspend fun deleteCalculationById(id: Long)
+
+    @Query("DELETE FROM deposit_calculations")
+    suspend fun clearAllCalculations()
 }
