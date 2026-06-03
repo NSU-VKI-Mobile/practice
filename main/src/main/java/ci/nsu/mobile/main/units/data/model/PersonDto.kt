1ad9d9a29153533b82286e427a1a5fb7ba22f83f
MaksimovskayaTE <maksimovskayate@mer.ci.nsu.ru>
2026-06-03 15:11:16 +0700

    несоответствие с сайтом

--- main/src/main/java/ci/nsu/mobile/main/units/data/model/PersonDto.kt
+++ main/src/main/java/ci/nsu/mobile/main/units/data/model/PersonDto.kt
@@ -1,13 +1,14 @@
 package ci.nsu.mobile.main.units.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class PersonDto(
-    val firstName: String,
-    val lastName: String,
-    val middleName: String,
-    val birthDate: String,
-    val gender: String,
-    val groupId: Int
+    @SerialName("firstName") val firstName: String,
+    @SerialName("lastName") val lastName: String,
+    @SerialName("middleName") val middleName: String,
+    @SerialName("birthDate") val birthDate: String,
+    @SerialName("gender") val gender: String,
+    @SerialName("groupId") val groupId: Int
 )
--- main/src/main/java/ci/nsu/mobile/main/units/data/model/UserDto.kt
+++ main/src/main/java/ci/nsu/mobile/main/units/data/model/UserDto.kt
@@ -1,13 +1,14 @@
 package ci.nsu.mobile.main.units.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserDto(
-    val id: Int,
-    val login: String,
-    val email: String,
-    val phoneNumber: String,
-    val roleId: Int,
-    val person: PersonDto? = null
+    @SerialName("userId") val id: Int,
+    @SerialName("login") val login: String,
+    @SerialName("email") val email: String,
+    @SerialName("phoneNumber") val phoneNumber: String,
+    @SerialName("roleId") val roleId: Int,
+    @SerialName("person") val person: PersonDto? = null
 )
